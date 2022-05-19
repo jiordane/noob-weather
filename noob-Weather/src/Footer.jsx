8d258f0ce2fb07ce34fc4f7c5577e6657cdb33ed
jiordane <jiordane@gmail.com>
2022-05-19 20:05:04 -0300

    noob-Weather

---
+++ noob-Weather/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => (
+  <div className='footer'>
+    ©Noob, 2022.
+
+  </div>
+);
+
---
+++ noob-Weather/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+export const Navbar = () => {
+  return (
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center">
+       <div className='active'>
+            
+              <p className='noob'>
+              ⛅️ Noob 
+              </p>
+           
+            
+       </div>
+       
+    </div>
+  );
+}
